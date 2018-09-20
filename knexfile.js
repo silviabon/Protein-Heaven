2c7c600d5ab0fc223dee1fdc2626fc9038025126
erik-mackie <erik.mackie@gmail.com>
2018-09-20 01:33:30 +0000

    knex file, changed db_port too port

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
       user     : process.env.DB_USER,
       password : process.env.DB_PASS,
       database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
+      port     : process.env.PORT,
       ssl      : process.env.DB_SSL
     },
     migrations: {
