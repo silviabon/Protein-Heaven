4690e8c6da8e504a931782dde5feca305524c5bf
erik-mackie <erik.mackie@gmail.com>
2018-09-21 18:43:31 +0000

    removed testing changes to app.js

--- db/migrations/20180920195533_menu_items.js
+++ db/migrations/20180920195533_menu_items.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('menu_items', function (t) {
     t.increments('id').primary();
+    t.string('name');
     t.decimal('price');
   })
 };
--- db/seeds/4_menu_item.js
+++ db/seeds/4_menu_item.js
@@ -7,14 +7,17 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('menu_items').insert({
           id: 1,
+          name: 'chicken',
           price: 12.99
         }),
         knex('menu_items').insert({
           id: 2,
+          name: 'beef',
           price: 15.99
         }),
         knex('menu_items').insert({
           id: 3,
+          name: 'fish',
           price: 19.99
         })
       ]);
