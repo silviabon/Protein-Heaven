c2d4f81b7bd6812f55bc7012e0e264f8c785ad81
silviabon <41654136+silviabon@users.noreply.github.com>
2018-10-09 20:34:19 -0700

    Update data-helpers.js
    Cleaning code

--- data/data-helpers.js
+++ data/data-helpers.js
@@ -4,13 +4,11 @@ function dataHelperMaker(knex){
   function getOrders(){
     const query = knex('orders')
       .select('*');
-    //console.log(query.toSQL());
     return query;
   }
   function getMenuItems(){
     const query = knex('items')
       .select('*');
-    //console.log(query.toSQL());
     return query;
   }
   return {
